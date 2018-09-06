efb53471da0e3818e48cbb5acd78a87bece15c1d
Chandan Kumar <chandanchaudhary@outlook.com>
2018-09-06 16:45:52 +0530

    Fetch students from details page if data is not present in store Show error message if roll no is not available

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { fetchStudents } from 'actions';
 import BarChart from 'components/BarChart';
 import Marksheet from 'components/Marksheet';
 
@@ -9,29 +10,60 @@ import './Student.css';
 
 class Student extends Component {
   state = {
-    student: null
+    student: null,
+    invalid: false
   };
 
   componentDidMount() {
-    const studentId = this.props.match.params.id;
-    const student = this.props.students[studentId];
-    this.setState({ student });
+    this.studentId = this.props.match.params.id;
+    const count = Object.keys(this.props.students).length;
+
+    if (count === 0) {
+        this.props.fetchStudents();
+    } else {
+      const student = this.props.students[this.studentId];
+      if(student) {
+        this.setState({ student });
+      } else {
+        this.setState({ invalid: true });
+      }
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const student = nextProps.students[this.studentId];
+    if (student) {
+      this.setState({ student });
+    } else {
+      this.setState({ invalid: true });
+    }
+  }
+
+  renderStudentDetails = () => {
+    const { student, invalid } = this.state;
+    if (student) {
+      return (
+        <Fragment>
+          <BarChart data={student} />
+          <Marksheet data={student} />
+        </Fragment>
+      );
+    } else {
+      if (invalid) {
+        return <div className="error">There is no student present with this Roll no.</div>;
+      } else {
+        return <div className="loading">Loading...</div>;
+      }
+    }
   }
 
   render() {
-    const { student } = this.state;
+    const { student, invalid } = this.state;
 
     return (
       <div className="student-details">
         <Link to="/">DashBoard</Link>
-        { student ?
-          (
-            <Fragment>
-              <BarChart data={student} />
-              <Marksheet data={student} />
-            </Fragment>
-          ) : null
-        }
+        { this.renderStudentDetails() }
       </div>
     );
   }
@@ -41,4 +73,4 @@ const mapStateToProps = ({ students }) => ({
   students
 });
 
-export default connect(mapStateToProps)(Student);
+export default connect(mapStateToProps, { fetchStudents })(Student);
